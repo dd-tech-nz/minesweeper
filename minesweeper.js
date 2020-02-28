7a25a5df702972ceaec41aa9556f0d5419a8506f
dd-tech-nz <ddumolo@hotmail.com>
2020-02-28 22:30:12 +1300

    complete countSurroundingMines function

--- minesweeper.js
+++ minesweeper.js
@@ -23,6 +23,9 @@ var board = {
 }
 
 function startGame() {
+  for (var i = 0; i < board.cells.length; i++) {
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
+  }
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
@@ -47,5 +50,15 @@ function checkForWin() {
 // It will return cell objects in an array. You should loop through 
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines(cell) {
+  
+  var count = 0
+  var surrounding = lib.getSurroundingCells(cell.row, cell.col)
+  //console.log(surrounding)
+  for (var i = 0; i < surrounding.length; i++) {
+    surrounding[i].isMine ? count ++ : count
+          
+  }
+  
+  return count
 }
 
